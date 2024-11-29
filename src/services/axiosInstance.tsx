e366f2cfcf784b6312563d1743869c7be1450266
Anubhav <anubhavshavaran5678@gmail.com>
2024-11-29 15:50:04 +0530

    #58 Remove request timeout

--- src/services/axiosInstance.tsx
+++ src/services/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: 'http://142.93.248.91:3000/api',
-    timeout: 5000,
+    // timeout: 5000,
     headers: {
         'Accept': 'application/json, text/plain, */*',
         'X-Custom-Header': 'foobar'
