31ba986e7e2473029e502d73c74167bba14bcda3
Anubhav <anubhavshavaran5678@gmail.com>
2024-11-29 15:42:38 +0530

    #57 Fix the typo in Base URL and set the request timeout to 5 seconds.

--- src/services/axiosInstance.tsx
+++ src/services/axiosInstance.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://http://142.93.248.91:3000/api',
-    // timeout: 5000,
+    baseURL: 'http://142.93.248.91:3000/api',
+    timeout: 5000,
     headers: {
         'Accept': 'application/json, text/plain, */*',
         'X-Custom-Header': 'foobar'
