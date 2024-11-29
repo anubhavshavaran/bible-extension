d414cad31a59c6ab68ffa3a6c8d8678b996cea84
Anubhav <anubhavshavaran5678@gmail.com>
2024-11-29 15:38:54 +0530

    #56 Change the base URL for requests to the URL of droplet.

--- src/services/axiosInstance.tsx
+++ src/services/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://selahvie-backend.onrender.com/api',
+    baseURL: 'http://http://142.93.248.91:3000/api',
     // timeout: 5000,
     headers: {
         'Accept': 'application/json, text/plain, */*',
