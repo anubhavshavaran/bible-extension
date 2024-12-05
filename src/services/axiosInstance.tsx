ed6c28642292fe95a0790871cf6aa8a9e8db391b
Anubhav <anubhavshavaran5678@gmail.com>
2024-12-05 15:03:34 +0530

    #59 Change api base URL

--- src/services/axiosInstance.tsx
+++ src/services/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://142.93.248.91:3000/api',
+    baseURL: 'https://selahvie.life/api',
     // timeout: 5000,
     headers: {
         'Accept': 'application/json, text/plain, */*',
